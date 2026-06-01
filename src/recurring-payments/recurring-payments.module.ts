6bf2ac45615a1dd7b78190dc5eed693d1f6b9fa5
Roman Khussein <romanfarri@gmail.com>
2026-06-01 18:59:50 +0300

    fix: resolve DI error in RecurringPaymentsModule

--- src/recurring-payments/recurring-payments.module.ts
+++ src/recurring-payments/recurring-payments.module.ts
@@ -5,11 +5,17 @@ import {
 import { RecurringPaymentsService } from './recurring-payments.service';
 import { RecurringPaymentsController } from './recurring-payments.controller';
 import { RelationalRecurringPaymentsPersistenceModule } from './infrastructure/persistence/relational/relational-persistence.module';
+import { AccountsModule } from '../accounts/accounts.module';
+import { CategoriesModule } from '../categories/categories.module';
+import { TransactionsModule } from '../transactions/transactions.module';
 
 @Module({
   imports: [
     // do not remove this comment
     RelationalRecurringPaymentsPersistenceModule,
+    AccountsModule,
+    CategoriesModule,
+    TransactionsModule,
   ],
   controllers: [RecurringPaymentsController],
   providers: [RecurringPaymentsService],
