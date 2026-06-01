142bf1fa7b152f4a798fc5e65e5fb2dbd4a28e7e
Roman Khussein <romanfarri@gmail.com>
2026-06-02 01:00:21 +0300

    feat: add logger

--- src/database/seeds/relational/run-seed.ts
+++ src/database/seeds/relational/run-seed.ts
@@ -1,29 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { RoleSeedService } from './role/role-seed.service';
 import { SeedModule } from './seed.module';
 import { StatusSeedService } from './status/status-seed.service';
 import { UserSeedService } from './user/user-seed.service';
 
 const runSeed = async () => {
-  console.log('Starting Database Seeding Process...');
+  const logger = new Logger('Seed');
+  logger.log('Starting Database Seeding Process...');
 
-  console.log('Bootstrapping SeedModule...');
+  logger.log('Bootstrapping SeedModule...');
   const app = await NestFactory.create(SeedModule);
 
-  console.log('Running Role Seed...');
+  logger.log('Running Role Seed...');
   await app.get(RoleSeedService).run();
-  console.log('Role Seed Completed!');
+  logger.log('Role Seed Completed!');
 
-  console.log('Running Status Seed...');
+  logger.log('Running Status Seed...');
   await app.get(StatusSeedService).run();
-  console.log('Status Seed Completed!');
+  logger.log('Status Seed Completed!');
 
-  console.log('Running User Seed...');
+  logger.log('Running User Seed...');
   await app.get(UserSeedService).run();
-  console.log('User Seed Completed!');
+  logger.log('User Seed Completed!');
 
   await app.close();
-  console.log('All Seeding Tasks Finished Successfully!');
+  logger.log('All Seeding Tasks Finished Successfully!');
 };
 
 void runSeed();
