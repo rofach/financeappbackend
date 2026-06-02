fec6eae19453f29f40e95e0a8a3cba95389ac864
Roman Khussein <romanfarri@gmail.com>
2026-06-03 01:07:03 +0300

    fix: migrations

---
+++ src/database/migrations/1780437874374-Init.ts
@@ -0,0 +1,243 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Init1780437874374 implements MigrationInterface {
+  name = 'Init1780437874374';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "role" ("id" integer NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_b36bcfe02fc8de3c57a8b2391c2" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "status" ("id" integer NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_e12743a7086ec826733f54e1d95" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "file" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "path" character varying NOT NULL, CONSTRAINT "PK_36b46d232307066b3a2c9ea3a1d" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "currency" ("code" character varying NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_723472e41cae44beb0763f4039c" PRIMARY KEY ("code"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "email" character varying, "password" character varying, "provider" character varying NOT NULL DEFAULT 'email', "socialId" character varying, "firstName" character varying, "lastName" character varying, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "photoId" uuid, "roleId" integer, "baseCurrencyCode" character varying, "statusId" integer, CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "REL_75e2be4ce11d447ef43be0e374" UNIQUE ("photoId"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_9bd2fe7a8e694dedc4ec2f666f" ON "user" ("socialId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_58e4dbff0e1a32a9bdc861bb29" ON "user" ("firstName") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_f0e1b4ecdca13b177e2e3a0613" ON "user" ("lastName") `,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "account" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "balance" numeric(15,2) NOT NULL DEFAULT '0', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "user_id" integer, "currency" character varying, CONSTRAINT "PK_54115ee388cdb6d86bb4bf5b2ea" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_efef1e5fdbe318a379c06678c5" ON "account" ("user_id") `,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "category" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "nameEn" character varying, "nameUk" character varying, "type" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "user_id" integer, CONSTRAINT "PK_9c4e4a89e3674fc9f382d733f03" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_6562e564389d0600e6e243d960" ON "category" ("user_id") `,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."transaction_type_enum" AS ENUM('INCOME', 'EXPENSE')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "transaction" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "type" "public"."transaction_type_enum" NOT NULL, "amount" numeric(12,2) NOT NULL, "baseAmount" numeric(12,2) NOT NULL, "date" TIMESTAMP NOT NULL, "note" text, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "user_id" integer, "account_id" uuid, "category_id" uuid, CONSTRAINT "PK_89eadb93a89810556e1cbcd6ab9" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_b4a3d92d5dde30f3ab5c34c586" ON "transaction" ("user_id") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_e2652fa8c16723c83a00fb9b17" ON "transaction" ("account_id") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_abbe63b71ee4193f61c322ab49" ON "transaction" ("category_id") `,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "budget" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "limitAmount" numeric(10,2) NOT NULL, "period" integer NOT NULL, "startDate" date NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "userId" integer, "categoryId" uuid, CONSTRAINT "PK_9af87bcfd2de21bd9630dddaa0e" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "session" ("id" SERIAL NOT NULL, "hash" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "userId" integer, CONSTRAINT "PK_f55da76ac1c3ac420f444d2ff11" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_3d2f174ef04fb312fdebd0ddc5" ON "session" ("userId") `,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."recurring_payments_type_enum" AS ENUM('INCOME', 'EXPENSE')`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."recurring_payments_frequency_enum" AS ENUM('DAILY', 'WEEKLY', 'MONTHLY', 'YEARLY')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "recurring_payments" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "type" "public"."recurring_payments_type_enum" NOT NULL, "amount" numeric(12,2) NOT NULL, "frequency" "public"."recurring_payments_frequency_enum" NOT NULL, "beginDate" date NOT NULL, "nextExecuteDate" date, "isActive" boolean NOT NULL DEFAULT true, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "user_id" integer, "account_id" uuid, "category_id" uuid, CONSTRAINT "PK_bbce8e2920bd6ee89a7c4ebf78a" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_be05009d1066df0465fd9ef6a0" ON "recurring_payments" ("user_id") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_6bbedb5ebe1e11b205843ec566" ON "recurring_payments" ("account_id") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_bc8e80b18873a3892da401a9d2" ON "recurring_payments" ("category_id") `,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "currency_rate" ("baseCode" character varying NOT NULL, "rates" jsonb, "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_6c1dc18f0f1d955a154c363b44a" PRIMARY KEY ("baseCode"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "FK_75e2be4ce11d447ef43be0e374f" FOREIGN KEY ("photoId") REFERENCES "file"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "FK_c28e52f758e7bbc53828db92194" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "FK_5d9fd3178ace5cde874a4125f5a" FOREIGN KEY ("baseCurrencyCode") REFERENCES "currency"("code") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "FK_dc18daa696860586ba4667a9d31" FOREIGN KEY ("statusId") REFERENCES "status"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "account" ADD CONSTRAINT "FK_efef1e5fdbe318a379c06678c51" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "account" ADD CONSTRAINT "FK_1bd607c3a38dcdfb1963da0bda2" FOREIGN KEY ("currency") REFERENCES "currency"("code") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "category" ADD CONSTRAINT "FK_6562e564389d0600e6e243d9604" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transaction" ADD CONSTRAINT "FK_b4a3d92d5dde30f3ab5c34c5862" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transaction" ADD CONSTRAINT "FK_e2652fa8c16723c83a00fb9b17e" FOREIGN KEY ("account_id") REFERENCES "account"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transaction" ADD CONSTRAINT "FK_abbe63b71ee4193f61c322ab497" FOREIGN KEY ("category_id") REFERENCES "category"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "budget" ADD CONSTRAINT "FK_8ed65c868c97a5fb471d85efb01" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "budget" ADD CONSTRAINT "FK_4aeadf37446801c8f4d2d26441b" FOREIGN KEY ("categoryId") REFERENCES "category"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "session" ADD CONSTRAINT "FK_3d2f174ef04fb312fdebd0ddc53" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "recurring_payments" ADD CONSTRAINT "FK_be05009d1066df0465fd9ef6a0c" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "recurring_payments" ADD CONSTRAINT "FK_6bbedb5ebe1e11b205843ec566d" FOREIGN KEY ("account_id") REFERENCES "account"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "recurring_payments" ADD CONSTRAINT "FK_bc8e80b18873a3892da401a9d2f" FOREIGN KEY ("category_id") REFERENCES "category"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "recurring_payments" DROP CONSTRAINT "FK_bc8e80b18873a3892da401a9d2f"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "recurring_payments" DROP CONSTRAINT "FK_6bbedb5ebe1e11b205843ec566d"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "recurring_payments" DROP CONSTRAINT "FK_be05009d1066df0465fd9ef6a0c"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "session" DROP CONSTRAINT "FK_3d2f174ef04fb312fdebd0ddc53"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "budget" DROP CONSTRAINT "FK_4aeadf37446801c8f4d2d26441b"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "budget" DROP CONSTRAINT "FK_8ed65c868c97a5fb471d85efb01"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transaction" DROP CONSTRAINT "FK_abbe63b71ee4193f61c322ab497"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transaction" DROP CONSTRAINT "FK_e2652fa8c16723c83a00fb9b17e"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transaction" DROP CONSTRAINT "FK_b4a3d92d5dde30f3ab5c34c5862"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "category" DROP CONSTRAINT "FK_6562e564389d0600e6e243d9604"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "account" DROP CONSTRAINT "FK_1bd607c3a38dcdfb1963da0bda2"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "account" DROP CONSTRAINT "FK_efef1e5fdbe318a379c06678c51"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "FK_dc18daa696860586ba4667a9d31"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "FK_5d9fd3178ace5cde874a4125f5a"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "FK_c28e52f758e7bbc53828db92194"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "FK_75e2be4ce11d447ef43be0e374f"`,
+    );
+    await queryRunner.query(`DROP TABLE "currency_rate"`);
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_bc8e80b18873a3892da401a9d2"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_6bbedb5ebe1e11b205843ec566"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_be05009d1066df0465fd9ef6a0"`,
+    );
+    await queryRunner.query(`DROP TABLE "recurring_payments"`);
+    await queryRunner.query(
+      `DROP TYPE "public"."recurring_payments_frequency_enum"`,
+    );
+    await queryRunner.query(
+      `DROP TYPE "public"."recurring_payments_type_enum"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_3d2f174ef04fb312fdebd0ddc5"`,
+    );
+    await queryRunner.query(`DROP TABLE "session"`);
+    await queryRunner.query(`DROP TABLE "budget"`);
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_abbe63b71ee4193f61c322ab49"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_e2652fa8c16723c83a00fb9b17"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_b4a3d92d5dde30f3ab5c34c586"`,
+    );
+    await queryRunner.query(`DROP TABLE "transaction"`);
+    await queryRunner.query(`DROP TYPE "public"."transaction_type_enum"`);
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_6562e564389d0600e6e243d960"`,
+    );
+    await queryRunner.query(`DROP TABLE "category"`);
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_efef1e5fdbe318a379c06678c5"`,
+    );
+    await queryRunner.query(`DROP TABLE "account"`);
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_f0e1b4ecdca13b177e2e3a0613"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_58e4dbff0e1a32a9bdc861bb29"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_9bd2fe7a8e694dedc4ec2f666f"`,
+    );
+    await queryRunner.query(`DROP TABLE "user"`);
+    await queryRunner.query(`DROP TABLE "currency"`);
+    await queryRunner.query(`DROP TABLE "file"`);
+    await queryRunner.query(`DROP TABLE "status"`);
+    await queryRunner.query(`DROP TABLE "role"`);
+  }
+}
